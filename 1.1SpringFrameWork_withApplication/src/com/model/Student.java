6dc1fee2da1ef129f3e218e5ae60a0ee2817d4ef
bhaiyya15 <bhaiyya.dandge15@gmail.com>
2023-10-25 09:53:09 +0530

    vhvhjh

---
+++ 1.1SpringFrameWork_withApplication/src/com/model/Student.java
@@ -0,0 +1,20 @@
+package com.model;
+
+public class Student 
+{
+
+	public Student() {
+		System.out.println("this is Student class constructor");
+
+	}
+	
+	public void display()
+	{
+		System.out.println("display method call");
+	}
+	
+	
+	
+	
+	
+}
